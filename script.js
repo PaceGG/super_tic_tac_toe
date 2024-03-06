197d9ad2b492adefc0db08e833274d5b116ab00a
PaceGG <yura3012004@gmail.com>
2024-03-06 18:22:57 +0500

    movement valid check

--- script.js
+++ script.js
@@ -1,39 +1,110 @@
 const cellsArray = document.querySelectorAll(".little-cell");
+const gameBoard = [
+  [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+  ],
+  [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+  ],
+  [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+  ],
+  [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+  ],
+  [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+  ],
+  [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+  ],
+  [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+  ],
+  [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+  ],
+  [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+  ],
+];
 
 const players = {
   x: "x",
   o: "o",
 };
 
-let currentPlayer = players.x;
+const currentPlayerDiv = document.querySelector(".current-player");
+let currentPlayerText = currentPlayerDiv.innerHTML;
 
-for (let i = 0; i < cellsArray.length; i++) {
-  cellsArray[i].addEventListener("click", function () {
-    if (!this.classList.contains("blocked")) {
-      if (currentPlayer === players.x) {
-        this.innerHTML = '<img src="img/x.png" alt="X" />';
-        currentPlayer = players.o;
-      } else {
-        this.innerHTML = '<img src="img/o.png" alt="X" />';
-        currentPlayer = players.x;
+// console.log(currentPlayerText);
+
+function renderBoard() {
+  let currentPlayer = players.x;
+  let previousMovement = null;
+
+  for (let i = 0; i < cellsArray.length; i++) {
+    cellsArray[i].addEventListener("click", function () {
+      if (
+        !this.classList.contains("blocked") &&
+        ((previousMovement * 9 <= i && i <= previousMovement * 9 + 8) ||
+          previousMovement === null)
+      ) {
+        if (currentPlayer === players.x) {
+          this.innerHTML = '<img src="img/x.png" alt="X" />';
+          currentPlayerText = 'Ход: <img src="img/o.png" alt="" />';
+          gameBoard[Math.floor(i / 9)][Math.floor((i % 9) / 3)][
+            Math.floor((i % 9) % 3)
+          ] = currentPlayer;
+          currentPlayer = players.o;
+        } else {
+          this.innerHTML = '<img src="img/o.png" alt="X" />';
+          currentPlayerText = 'Ход: <img src="img/x.png" alt="" />';
+          gameBoard[Math.floor(i / 9)][Math.floor((i % 9) / 3)][
+            Math.floor((i % 9) % 3)
+          ] = currentPlayer;
+          currentPlayer = players.x;
+        }
+        currentPlayerDiv.innerHTML = currentPlayerText;
+        this.classList.add("blocked");
+        previousMovement = i % 9;
       }
-      this.classList.add("blocked");
-    }
-  });
+      console.log(i);
+      console.log(gameBoard);
+    });
+  }
 }
 
-function massive_create() {
-  const big_cells = new Array(9).fill("");
+// function massive_create() {
+//   const big_cells = new Array(9).fill("");
 
-  const top_left_cell = new Array(9).fill("");
-  const top_cell = new Array(9).fill("");
-  const top_right_left_cell = new Array(9).fill("");
-  const left_cell = new Array(9).fill("");
-  const center_cell = new Array(9).fill("");
-  const right_cell = new Array(9).fill("");
-  const bottom_left_cell = new Array(9).fill("");
-  const bottom_cell = new Array(9).fill("");
-  const bottom_right_cell = new Array(9).fill("");
+//   const top_left_cell = new Array(9).fill("");
+//   const top_cell = new Array(9).fill("");
+//   const top_right_left_cell = new Array(9).fill("");
+//   const left_cell = new Array(9).fill("");
+//   const center_cell = new Array(9).fill("");
+//   const right_cell = new Array(9).fill("");
+//   const bottom_left_cell = new Array(9).fill("");
+//   const bottom_cell = new Array(9).fill("");
+//   const bottom_right_cell = new Array(9).fill("");
 
-  console.log(big_cells);
-}
+//   console.log(big_cells);
+// }
